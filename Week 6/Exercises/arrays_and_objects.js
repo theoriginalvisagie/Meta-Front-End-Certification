1a1047f7dbe266c3d02621f125656207be89933a
Christiaan Visagie <visagiechristiaan40@gmail.com>
2022-12-07 17:26:28 +0200

    Week 6

---
+++ Week 6/Exercises/arrays_and_objects.js
@@ -0,0 +1,17 @@
+var clothes = [];
+clothes.push("Shoes");
+clothes.push("Shirts");
+clothes.push("Pants");
+clothes.push("Jacket");
+clothes.push("Hats");
+
+clothes.pop();
+clothes.push("Glasses");
+
+console.log(clothes[2]);
+
+var favCar = {};
+favCar.color = "Blue";
+favCar.convertible = false;
+
+console.log(favCar);
