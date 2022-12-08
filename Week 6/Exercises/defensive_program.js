2c870ac36f7597428ff0f23685bc37a595d7f8c9
Christiaan Visagie <visagiechristiaan40@gmail.com>
2022-12-08 18:57:52 +0200

    Week 6 and 7

---
+++ Week 6/Exercises/defensive_program.js
@@ -0,0 +1,19 @@
+function letterFinder(word, match) {
+    let condition1 = typeof(word) == 'string' && word.length >= 2;
+    let condition2 = typeof(match) == 'string' && match.length == 1;
+
+    if(condition1 && condition2){
+        for(let i = 0; i < word.length; i++) {
+            if(word[i] == match) {
+                console.log('Found the', match, 'at', i)
+            } else {
+                console.log('---No match found at', i)
+            }
+        }
+    }else{
+        console.log("Please pass correct arguments to the function.");
+    }
+    
+}
+
+letterFinder("cat","c");
---
+++ Week 7/exercises/funcitonal_programming.js
@@ -0,0 +1,10 @@
+var shoes = 100;
+var stateTax = 1.2;
+
+function totalPrice(shoes, tax){
+    return shoes * tax;
+}
+
+var toPay = totalPrice(shoes, stateTax);
+
+console.log(toPay);
