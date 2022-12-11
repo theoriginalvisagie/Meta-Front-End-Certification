8d405e42b8732d38c4ee645e7321414d4b99a820
Christiaan Visagie <visagiechristiaan40@gmail.com>
2022-12-11 15:06:15 +0200

    TDD first exercises and labs

---
+++ Week 8/Labs/timesTwo.js
@@ -0,0 +1,10 @@
+const { timeStamp } = require("console");
+
+// Task 1: Code the timesTwo function declaration
+function timesTwo(val){
+    return 2 * val;
+}
+
+
+// Task 2: Export the timesTwo function as a module
+module.exports = timesTwo;
