1d6852ca63f1e7836099e58b1d67bcab2d63bf8b
Christiaan Visagie <67701001+theoriginalvisagie@users.noreply.github.com>
2023-01-17 11:15:46 +0000

    composition exercises

---
+++ Course 6 - Advanced React/Week 3/Exercises/composition-and-specialization/src/App.js
@@ -0,0 +1,34 @@
+import logo from './logo.svg';
+import './App.css';
+
+const Button = ({children, backgroundColor}) => {
+  return <button style={backgroundColor}>{children}</button>
+};
+
+const Alert = ({children}) =>{
+  return (
+    <>
+      <div className='Overlay' />
+      <div className='Alert'>{children}</div>
+    </>
+  );
+}
+
+const DeleteButton = () => {
+  return <Button backgroundColor="red">Delete</Button>;
+}
+
+function App() {
+  return (
+    <div className="App">
+      <header>My Restaurant</header>
+      <Alert>
+        <h4>Delete Account</h4>
+        <p>Are you sure you want to proceed?</p>
+        <DeleteButton />
+      </Alert>
+    </div>
+  );
+}
+
+export default App;
