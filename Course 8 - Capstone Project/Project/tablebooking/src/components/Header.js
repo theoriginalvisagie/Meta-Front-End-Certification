c7b391ef37143fb9d5645407041490ea0476bc95
Christiaan Visagie <visagiechristiaan40@gmail.com>
2023-02-08 09:26:40 +0200

    Meta Tags added

--- Course 8 - Capstone Project/Project/tablebooking/src/components/Header.js
+++ Course 8 - Capstone Project/Project/tablebooking/src/components/Header.js
@@ -1,7 +1,12 @@
+import Logo_small from '../images/Logo_small.png';
+
 function Header(){
     return(
         <header>
-            
+            <meta name="description" content="The Best Restaurant to ever exist."/>
+            <meta name="og:title" content="Extraordinary Food"/>
+            <meta name="og:description" content="The Best Restaurant to ever exist."/>
+            <meta name="og:image" content={Logo_small}/>
         </header>
     );
 }
