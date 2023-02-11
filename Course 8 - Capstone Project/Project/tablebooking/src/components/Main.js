7df0dea3bdfa64803ab5a9c65689ea16c49c1dbb
Christiaan Visagie <visagiechristiaan40@gmail.com>
2023-02-11 16:21:42 +0200

    Styling updates

--- Course 8 - Capstone Project/Project/tablebooking/src/components/Main.js
+++ Course 8 - Capstone Project/Project/tablebooking/src/components/Main.js
@@ -1,6 +1,18 @@
+import '../App.css';
+import image from '../images/Hero_image.jpg';
+
 function Main(){
     return(
         <main className='main'>
+            <div>
+                <h1>Little Lemon</h1>
+                <h3>Chicago</h3>
+                <p>Description of food that the little lemon restaurant offers to its customers in a timely and quality way.</p>          
+                <button className='button'>Reserve A Table</button>
+            </div>
+            <div>
+                <img src={image} alt='Hero Section Image' />
+            </div>
             
         </main>
     );
--- Course 8 - Capstone Project/Project/tablebooking/src/components/Nav.js
+++ Course 8 - Capstone Project/Project/tablebooking/src/components/Nav.js
@@ -1,10 +1,11 @@
 import '../App.css';
+import Logo from '../images/Logo_small.png';
 
 function Nav(){
     const menuItems = ["Home","About","Menu","Reservations","Order Online","Login"];
     return(
         <nav className='nav'>
-            <img src="" alt=""></img>
+            <img src={Logo} alt="Little Lemon Logo" id="navLogo"></img>
             <ul className="menuItems">
                 {
                     menuItems.map((item)=>{
